3415ed9972722074ec427b558c5f0c4fad16d829
JorgeCelia <jorge.garcia@iescelia.org>
2026-04-23 18:22:28 +0200

    Add files via upload

---
+++ Holamundo.java
@@ -0,0 +1,8 @@
+public class Holamundo {
+    /* Programa holamundo */
+    public static void main(String[] args) {
+    // Lo único que hace este programa es mostrar un saludo por la pantalla
+    System.out.print("Hola, mundo!");
+    }
+}
+    
